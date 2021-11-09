2ea7b6b133287c71355900280184d516d11a5aa1
matt-pearson29 <matt.pearson29@googlemail.com>
2021-11-09 18:52:46 +0000

    upgrade

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,7 +1,7 @@
 function sendMail(contactForm) {
     emailjs.send("gmail", "resume", {
         "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
+        "from_email": contactForm.email.value,
         "project_request": contactForm.projectsummary.value
     })
     .then(
